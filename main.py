42e2647e654b3ad1cac0b725cc0715e6d5fdad0e
viargentum <avischetlin@gmail.com>
2021-05-10 23:16:40 -0400

    Create command parser, impliment simple best move functionality

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import time
@@ -5,30 +7,39 @@ from collections import Counter
 
 import tweepy
 from persistqueue import Queue
+from stockfish import Stockfish
 
+# grab keys from environment variables
 consumer_key = os.getenv("TWT_API_KEY")
 consumer_secret = os.getenv("TWT_API_SECRET")
 access_token = os.getenv("TWT_ACCESS_TOKEN")
 access_secret = os.getenv("TWT_ACCESS_SECRET")
 
+# create auth & api objects
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-mention_queue = Queue("queue")
+# create stockfish object
+stockfish = Stockfish()
 
+# create empty queue
+tweet_queue = Queue("queue")
+
+
+# persists prev_id to avoid reprocessing mentions
 try:
-    with open("id.txt", "r") as idFile:
-        lastId = idFile.read()
+    with open("id.txt", "r") as idfile:
+        prev_id = idfile.read()
 
 except FileNotFoundError:
-    lastId = None
+    prev_id = None
 
 
 def update_id(id):
-    with open("id.txt", "w") as idFile:
-        idFile.write(id)
+    with open("id.txt", "w") as idfile:
+        idfile.write(id)
 
 
 def __init__(args: str = None):
@@ -38,27 +49,33 @@ def __init__(args: str = None):
     get_mentions()
 
 
+# interates through mentions, adding those that fit the criteria to tweet_queue
 def get_mentions():
-    global mention_queue
-    global lastId
+    global tweet_queue
+    global prev_id
 
     while True:
+        print(tweet_queue.qsize())
         try:
+            # adds mention to list if it hasn't been processed
             mentions = (
                 api.mentions_timeline()
-                if lastId == None
-                else api.mentions_timeline(since_id=lastId)
+                if prev_id == None
+                else api.mentions_timeline(since_id=prev_id)
             )
 
             if len(mentions) > 0:
-                lastId = mentions[0].id_str
+                prev_id = mentions[0].id_str
 
                 for tweet in mentions:
-                    mention_queue.put(tweet)
-                    print(mention_queue.qsize())
+                    print(f"Mention from {tweet.author.name}")
 
-                update_id(lastId)
-                process_tweets()
+                    if "fen:" in tweet.text:
+                        tweet_queue.put(tweet)
+                        print(tweet_queue.qsize())
+
+                update_id(prev_id)
+                handle_tweets()
 
         except Exception as e:
             print(e)
@@ -66,18 +83,44 @@ def get_mentions():
         time.sleep(20)
 
 
-def process_tweets():
-    global mention_queue
+def handle_tweets():
+    global tweet_queue
 
     while True:
+        print(tweet_queue.qsize())
 
         try:
-            tweet = mention_queue.get()
-            api.update_status(
-                "recieved",
-                in_reply_to_status_id=tweet.id,
-                auto_populate_reply_metadata=True,
-            )
+            tweet = tweet_queue.get()
+            index = tweet.text.index("fen:") + 4
+            substr = tweet.text[index:]
+
+            if "fen:" in substr:
+                api.update_status(
+                    "Your tweet contained multiple FEN keywords.",
+                    in_reply_to_status_id=tweet.id,
+                    auto_populate_reply_metadata=True,
+                )
+                tweet_queue.task_done()
+
+            else:
+                stockfish.set_fen_position(substr)
+
+                try:
+                    best_move = str(stockfish.get_best_move())
+                    api.update_status(
+                        f"Best move is probably: {best_move}",
+                        in_reply_to_status_id=tweet.id,
+                        auto_populate_reply_metadata=True,
+                    )
+                    tweet_queue.task_done()
+
+                except BrokenPipeError:
+                    api.update_status(
+                        "Invalid FEN.",
+                        in_reply_to_status_id=tweet.id,
+                        auto_populate_reply_metadata=True,
+                    )
+                    tweet_queue.task_done()
 
         except Exception as e:
             print(e)
@@ -121,9 +164,9 @@ def cli_access():
 
 if __name__ == "__main__":
     try:
-        if sys.argv:
+        try:
             __init__(sys.argv[1])
-        else:
+        except IndexError:
             __init__()
 
     except KeyboardInterrupt:
